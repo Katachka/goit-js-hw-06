678a04a0a20377dc22660cabbdf95eefc2be8916
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-04 20:41:21 +0200

    1-2 tasks

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredList = document.querySelector('ul#ingredients')
+
+const igredEl = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = `${ingredient}`;
+  return li
+})
+
+ingredList.append(...igredEl);
+
+console.log(igredEl);
+
+
