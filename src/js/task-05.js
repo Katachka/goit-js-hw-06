22fd14831eff700a441acb14846751f078eda304
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-05 15:36:17 +0200

    task-05

---
+++ src/js/task-05.js
@@ -0,0 +1,12 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-output'),
+}
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    if (refs.input.value === "") {
+    return (refs.span.textContent = "Anonymous");
+  }
+  refs.span.textContent = event.currentTarget.value;
+}
