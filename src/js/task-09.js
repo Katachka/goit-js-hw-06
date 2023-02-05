0ea14a153eeba7618b830e277f80e6c6e29619a6
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-05 22:03:23 +0200

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', (event) => {
+ const randomColor = getRandomHexColor();
+  
+  spanColor.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
