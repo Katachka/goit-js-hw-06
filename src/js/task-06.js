b119b6b27bc0cc691cebb7b3eb0150377ba664ef
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-08 21:30:16 +0200

    task-06-(2)

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,8 +1,29 @@
+// const inputEl = document.querySelector('#validation-input');
+// inputEl.addEventListener('blur', onInputBlur);
+// function onInputBlur(event) {
+//     inputEl.classList.remove('invalid', 'valid');
+//     (inputEl.value.length === Number(inputEl.dataset.length)) ?
+//         inputEl.classList.add('valid') :
+//         inputEl.classList.add('invalid');
+// }
+// const inputEl = document.querySelector('#validation-input');
+// inputEl.addEventListener('blur', onInputBlur);
+// function onInputBlur(event) {
+//  inputEl.classList.remove('invalid', 'valid');
+//     if (inputEl.value.length === Number(inputEl.dataset.length)) {
+//         return inputEl.classList.add('valid');
+//     }
+//     else {
+//     return inputEl.classList.add('invalid');
+// }
+// }
 const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur);
+
 function onInputBlur(event) {
-    inputEl.classList.remove('invalid', 'valid');
-    (inputEl.value.length === Number(inputEl.dataset.length)) ?
-        inputEl.classList.add('valid') :
-        inputEl.classList.add('invalid');
+if (inputEl.value.length === Number(inputEl.dataset.length)) {
+        inputEl.classList.remove('invalid');
+        return inputEl.classList.add('valid');
+    }
+    return inputEl.classList.add('invalid');
 }
